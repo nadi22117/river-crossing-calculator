2dd2c5300d64c880e9ca3fea10b20ad87a3984ff
nadi22117 <nadavgrin2@gmail.com>
2025-05-05 08:47:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-from flask import Flask, request, jsonify
 from flask import Flask, request, jsonify, render_template
+from Heding import compute_crossing
 
-from Heding import compute_crossing  # מייבא את הפונקציה שכתבת קודם
+app = Flask(__name__)
 
-app = Flask(__name__)  # יוצר את השרת
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/calculate')
 def calculate():
-    # קולט את המשתנים מהכתובת (URL)
     vb = float(request.args.get('vb'))
     vr = float(request.args.get('vr'))
     theta = float(request.args.get('theta'))
     D = float(request.args.get('D'))
 
-    # מפעיל את פונקציית החישוב
     result = compute_crossing(vb, vr, theta, D)
-
-    # מחזיר את התוצאה בפורמט JSON
     return jsonify(result)
 
-# גורם לקובץ לרוץ רק אם מפעילים אותו ישירות (ולא בייבוא)
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=False, host='0.0.0.0', port=10000)
